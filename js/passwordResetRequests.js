6c5069d602f346504f2dd3e49e04bf932efd4f86
TheAlexBig <00062816@uca.edu.sv>
2019-05-09 19:31:06 -0600

    Update passwordResetRequests.js

--- js/passwordResetRequests.js
+++ js/passwordResetRequests.js
@@ -31,7 +31,7 @@ const resetPassword = () => {
     const p1 = password.value;
     const p2 = repassword.value;
     if(parameters){
-        fetch(`https://crese-asistencia.herokuapp.com/API/v1/user/reset-password?newPassword=${p1}+"&verifyPassword=${p2}`, 
+        fetch(`https://crese-asistencia.herokuapp.com/API/v1/user/reset-password?newPassword=${p1}&verifyPassword=${p2}`, 
         parameters).then(data=>{
             return data.status;
         }).then(res=>{
